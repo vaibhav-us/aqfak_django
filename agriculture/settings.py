b17636438d3eb5d8198c4046d85bf814b0f1b621
vaibhav-us <vaibhavkrishnavus@gmail.com>
2024-07-01 02:15:14 +0530

    0.134

--- agriculture/settings.py
+++ agriculture/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 
 import environ                          #envvariable addition
 import os                               #envvariable addition
-
+from dotenv import load_dotenv
+load_dotenv()
 
 
 
@@ -98,8 +99,12 @@ WSGI_APPLICATION = 'agriculture.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
