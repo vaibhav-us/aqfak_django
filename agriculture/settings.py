ec53f9f7262fd4321a8f7fb8e5fff4e773939287
vaibhav-us <vaibhavkrishnavus@gmail.com>
2024-07-16 10:08:16 +0530

    token implemented.

--- agriculture/settings.py
+++ agriculture/settings.py
@@ -90,6 +90,11 @@ TEMPLATES = [
         },
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # Add this line
+    ],
+}
 
 WSGI_APPLICATION = 'agriculture.wsgi.application'
 
