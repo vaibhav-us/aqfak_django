a3cb5f825959ebf40b2ec6eef291b94e13900d87
vaibhav-us <vaibhavkrishnavus@gmail.com>
2024-07-21 11:29:50 +0530

    staticfiles

--- agriculture/settings.py
+++ agriculture/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c&y=7d7l+ifml)=@e_!2#2a0w01zism2^^#%0-@#qi4y@e%m2s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -148,6 +148,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
